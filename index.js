64db1c1c3914c01dc2955022c0ff7a6c791cf56a
Axel LUCAS <74660628+craftybloxy@users.noreply.github.com>
2022-11-29 17:23:39 +0000

    added write to file

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-// init
+//init file stream
+const fs = require('fs');
+const writeStream = fs.createWriteStream('result.txt');
+const pathName = writeStream.path;
+
+
+// init prompt
 const prompt = require("prompt-sync")();
 // promt for both arrays and separate word with sapces
 let array1 = prompt("array1$ ").split(" ");
@@ -13,4 +19,21 @@ while (i < array1.length) {
   i += 1
 }
 
+// log out the resut
 console.log(result_array)
+
+//write line by line the result
+result_array.forEach(value => writeStream.write(`${value}\n`));
+
+// the finish event is emitted when all data has been flushed from the stream
+writeStream.on('finish', () => {
+   console.log(`wrote all the result_array data to file ${pathName}`);
+});
+
+// handle the errors on the write process
+writeStream.on('error', (err) => {
+    console.error(`There is an error writing the file ${pathName} => ${err}`)
+});
+
+// close the stream
+writeStream.end();
